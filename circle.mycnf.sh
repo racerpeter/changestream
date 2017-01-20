758efb2d1bedfb61c4833504bcd50f2650a0aed3
Peter Fry <peter@pcfry.com>
2017-01-20 01:15:03 -0800

    finally

--- circle.mycnf.sh
+++ circle.mycnf.sh
@@ -2,3 +2,5 @@
 
 [ "$CIRCLE_NODE_INDEX" == "0" ] && sudo cp -f src/test/resources/my.cnf /etc/my.cnf
 [ "$CIRCLE_NODE_INDEX" == "1" ] && sudo cp -f src/test/resources/my-gtid.cnf /etc/my.cnf
+
+exit 0
