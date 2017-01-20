f01b41e990814aae55bdb93fdd2baa17da920bae
Peter Fry <peter@pcfry.com>
2017-01-20 01:03:12 -0800

    damn circle

--- circle.mycnf.sh
+++ circle.mycnf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-[ "$CIRCLE_NODE_INDEX" == "0" ] && sudo mv src/test/resources/my.cnf /etc/my.cnf
-[ "$CIRCLE_NODE_INDEX" == "1" ] && sudo mv src/test/resources/my-gtid.cnf /etc/my.cnf
+[ "$CIRCLE_NODE_INDEX" == "0" ] && sudo cp -f src/test/resources/my.cnf /etc/my.cnf
+[ "$CIRCLE_NODE_INDEX" == "1" ] && sudo cp -f src/test/resources/my-gtid.cnf /etc/my.cnf
