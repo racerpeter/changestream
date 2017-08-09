ede5658c32df9d97fd02ad3c373982bc98847726
Peter Fry <peter@pcfry.com>
2017-08-08 17:52:54 -0700

    Use circle 2.0 (WIP)
    Need to implement separate test container for GTID repl

--- circle.mycnf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-[ "$CIRCLE_NODE_INDEX" == "0" ] && sudo cp -f src/test/resources/my.cnf /etc/my.cnf
-[ "$CIRCLE_NODE_INDEX" == "1" ] && sudo cp -f src/test/resources/my-gtid.cnf /etc/my.cnf
-
-exit 0
