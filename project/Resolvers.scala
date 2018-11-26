453400151982fca32387dce71cd1b532453a3381
Peter Fry <peter@pcfry.com>
2018-11-26 09:37:16 -0800

    Add kamon with prometheus plugin

--- project/Resolvers.scala
+++ project/Resolvers.scala
@@ -8,6 +8,7 @@ object Resolvers {
     "Spray Repository" at "http://repo.spray.io",
     Resolver.jcenterRepo,
     Resolver.sbtPluginRepo("sbt-plugin-releases"),
-    Resolver.bintrayRepo("mingchuno", "maven")
+    Resolver.bintrayRepo("mingchuno", "maven"),
+    Resolver.bintrayRepo("kamon-io", "sbt-plugins")
   )
 }
