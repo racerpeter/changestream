8ca8ae01ca229197777f5575572a3198be66b81f
Peter Fry <peter@pcfry.com>
2018-12-18 15:07:44 -0800

    Make art optional

--- src/main/scala/changestream/ChangeStream.scala
+++ src/main/scala/changestream/ChangeStream.scala
@@ -51,10 +51,10 @@ object ChangeStream extends App {
   def clientId = client.getServerId
   def isConnected = client.isConnected
 
-  def getConnectedAndWait(startingPosition: Option[String]) = Await.result(getConnected(startingPosition), 60.seconds)
+  def getConnectedAndWait(startingPosition: Option[String] = None) = Await.result(getConnected(startingPosition), 60.seconds)
   def disconnectClient = client.disconnect()
 
-  def getConnected(startingPosition: Option[String]) = {
+  def getConnected(startingPosition: Option[String] = None) = {
     log.info("Starting changestream...")
 
     val getPositionFuture = startingPosition match {
